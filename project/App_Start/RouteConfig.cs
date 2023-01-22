821896c3604064ab31511a732978ac430d2b3885
froye <royebsan@gmail.com>
2023-01-22 11:09:20 +0700

    Add project files.

---
+++ project/App_Start/RouteConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace project
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Home" }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Register" }
+            );
+
+            routes.MapRoute(
+               name: "User_Profile",
+               url: "{controller}/{action}",
+               defaults: new { controller = "Home", action = "User_Profile" }
+           );
+
+
+        }
+    }
+}
---
+++ project/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult User_Profile()
+        {
+            return View();
+        }
+    }
+}
